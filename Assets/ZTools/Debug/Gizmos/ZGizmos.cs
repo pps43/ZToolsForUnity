71c704c25aa813728a225bd8d189b1944dbb7ff1
jony <j.c.zhao@foxmail.com>
2019-01-09 16:04:05 +0800

    Gizmos Tool

---
+++ Assets/ZTools/Debug/Gizmos/ZGizmos.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace ZTools.Debug
+{
+    /// <summary>
+    /// draw gizmos with color
+    /// </summary>
+    public class ZGizmos
+    {
+        public static void DrawLine(Vector3 from, Vector3 to, Color newColor)
+        {
+            Color ori = Gizmos.color;
+            Gizmos.color = newColor;
+            Gizmos.DrawLine(from, to);
+            Gizmos.color = ori;
+        }
+
+        public static void DrawSphere(Vector3 center, float radius, Color newColor)
+        {
+            Color ori = Gizmos.color;
+            Gizmos.color = newColor;
+            Gizmos.DrawSphere(center, radius);
+            Gizmos.color = ori;
+        }
+    }
+}
