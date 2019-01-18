24087cae3cbf41274fed46b05ad2cdf9e26097e8
jony <j.c.zhao@foxmail.com>
2019-01-18 16:45:44 +0800

    Update GamePlay.cs
    Add temp structure

--- Assets/ZTools/GameWorld/ActorHierachy/GamePlay.cs
+++ Assets/ZTools/GameWorld/ActorHierachy/GamePlay.cs
@@ -1,18 +1,65 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class GamePlay : MonoBehaviour
+using ZTools.Event;
+using ZTools.FSM;
+using ZTools.Game.CollisionUtil;
+namespace ZTools.Game
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// Global singleton as an etry point, manage in-game modules/submanagers.
+    /// GamePlay itself has a FSM, which include loading, playing, pause, exit state.
+    /// </summary>
+    public class GamePlay : MonoBehaviour, IUpdatable
     {
-        
-    }
+        public event Action updateEvent;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public static GamePlay instance;
+
+        //public InputManager inputManager { get; private set; }
+        public EventDispatcher eventDispatcher { get; private set; }
+        //public LevelManager levelManager { get; private set; }
+        //public EnemyManager enemyManager { get; private set; }
+        public CollisionManager collisionManager { get; private set; }
+
+        private EventHelper _eventHelper;
+        public FSM<GamePlay, NoEvent, CommonEvent> fsm { get; private set; }
+
+        private void Awake()
+        {
+            instance = this;//temp
+        }
+
+        private void Start()
+        {
+            //fsm = FSMFactory.createFSM(this, new InitialState(), null);
+            fsm.start();
+        }
+
+        private void Update()
+        {
+            updateEvent?.Invoke();
+            eventDispatcher.update();
+        }
+
+
+        public void InitGamePlay()
+        {
+            //get managers
+
+            eventDispatcher = EventDispatcher.instance;
+
+            _eventHelper = new EventHelper();
+            //add listener
+        }
+
+        public void UnInitGamePlay()
+        {
+            fsm.stop(); fsm = null;
+
+            //remove listener
+
+        }
+
     }
 }
