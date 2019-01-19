c9f5b8142f3287ae9fb214fd306cbf7d0ea1b8a5
jony <j.c.zhao@foxmail.com>
2019-01-19 18:31:16 +0800

    Test Code

---
+++ Assets/Test/classA.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class classA : MonoBehaviour
+{
+    public static classA instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+    
+    public void dosth()
+    {
+        Debug.Log("class a call b");
+    }
+}
---
+++ Assets/Test/classB.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class classB : MonoBehaviour
+{
+    private void Awake()
+    {
+        classA.instance.dosth();
+    }
+    
+}
