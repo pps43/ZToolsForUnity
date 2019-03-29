c2adf592d4d7f98c83c8d11de6d216f90c685057
jony <j.c.zhao@foxmail.com>
2019-03-29 11:16:48 +0800

    ZGizmos
    drawArrow, drawCircle2D

--- Assets/ZTools/Debug/Gizmos/ZGizmos.cs
+++ Assets/ZTools/Debug/Gizmos/ZGizmos.cs
@@ -21,5 +21,39 @@ namespace ZTools.DebugUtil
             Gizmos.DrawSphere(center, radius);
             Gizmos.color = ori;
         }
+
+        public static void DrawArrow(Vector3 begin, Vector3 end, Color newColor)
+        {
+            Color ori = Gizmos.color;
+            Gizmos.color = newColor;
+
+            Gizmos.DrawLine(begin, end);
+
+            float arrowHeadAngle = 20;
+            float arrowHeadLength = 5;
+
+            Vector3 direction = end - begin;
+            Vector3 right = Quaternion.LookRotation(direction) * Quaternion.Euler(arrowHeadAngle, 0, 0) * Vector3.back;
+            Vector3 left = Quaternion.LookRotation(direction) * Quaternion.Euler(-arrowHeadAngle, 0, 0) * Vector3.back;
+            Vector3 up = Quaternion.LookRotation(direction) * Quaternion.Euler(0, arrowHeadAngle, 0) * Vector3.back;
+            Vector3 down = Quaternion.LookRotation(direction) * Quaternion.Euler(0, -arrowHeadAngle, 0) * Vector3.back;
+
+            Gizmos.DrawRay(end, right * arrowHeadLength);
+            Gizmos.DrawRay(end, left * arrowHeadLength);
+
+            Gizmos.color = ori;
+        }
+
+        public static void DrawCircle2D(Vector3 center, float radius, Color newColor)
+        {
+#if UNITY_EDITOR
+            Color ori = UnityEditor.Handles.color;
+            UnityEditor.Handles.color = newColor;
+
+            UnityEditor.Handles.DrawWireDisc(center, Vector3.back, radius);
+            UnityEditor.Handles.color = ori;
+#endif
+
+        }
     }
 }
