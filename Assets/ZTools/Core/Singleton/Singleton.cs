2600830c51aeb9b3334042cef5a72c05fbe46f7a
pps <j.c.zhao@foxmail.com>
2021-09-18 22:50:03 +0800

    Correct some mistakes of Singleton

--- Assets/ZTools/Core/Singleton/Singleton.cs
+++ Assets/ZTools/Core/Singleton/Singleton.cs
@@ -5,19 +5,19 @@
     /// Multithread-safe singleton of type T.
     /// </summary>
     /// <typeparam name="T">Your type.</typeparam>
-    public class Singleton<T>
+    public class Singleton<T> where T: new()
     {
         private Singleton() {}
         
 #if UNITY_2019_1_OR_NEWER
-        private static Lazy<Singleton<T>> _instance = new Lazy<Singleton<T>>(() => new Singleton<T>());
+        private static Lazy<T> _instance = new Lazy<T>(() => new T());
 
-        public static Singleton<T> Instance => _instance.Value;
+        public static T Instance => _instance.Value;
 #else
-        private static volatile Singleton<T> _instance;
+        private static T _instance;
         private static object _syncRoot = new object();
         
-        public static Singleton<T> Instance
+        public static T Instance
         {
             get 
             {
@@ -26,7 +26,7 @@
                     lock (_syncRoot) 
                     {
                         if (_instance == null) 
-                            _instance = new Singleton<T>();
+                            _instance = new T();
                     }
                 }
                 return _instance;
